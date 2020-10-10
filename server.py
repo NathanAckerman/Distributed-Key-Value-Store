6e0867321b6bc012971adc68682ee70d4806409d
Nathan Ackerman <nga5@pitt.edu>
2020-10-10 14:50:47 -0400

    started skeleton for server

---
+++ server.py
@@ -0,0 +1,55 @@
+import sys
+from flask import *
+from flask_restful import Resource, Api
+
+#setup for flask
+app = Flask(__name__)
+api = Api(app)
+the_dict = {}
+
+python server.py –host <hostip> –port <xxxx> -zookeeper <zkip> -zookeeper_port
+<zkport>
+
+host_ip = sys.argv[2]
+host_port = sys.argv[4]
+zkip = sys.argv[6]
+zkport = sys.argv[7]
+
+class ReadValue(Resource):
+    def get(self, the_key):
+        if the_key in the_dict:
+            return the_dict[the_key]
+        else:
+            return None
+
+
+def set_new_value_globally(the_key, the_value):
+    return "TODO"
+
+def send_new_value_to_leader(the_key, the_val):
+    return TODO
+
+class AddUpdateValue(Resource):
+    def get(self, the_key, the_val):
+        return send_new_value_to_leader(the_key, the_val)
+        
+
+class AUVForLeader(Resource):
+    def get(self, the_key, the_val):
+        return set_new_value_globally(the_key, the_val)
+        
+
+class AUVFromLeader(Resource):
+    def get(self, the_key, the_val):
+        the_dict[the_key] = the_val
+
+
+
+api.add_resource(ReadValue, '/<string:the_key>')
+api.add_resource(AddUpdateValue, '/<string:the_key>/<string:the_val>')
+api.add_resource(AUVForLeader, '/<string:the_key>/<string:the_val>')
+api.add_resource(AUVFromLeader, '/<string:the_key>/<string:the_val>')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
